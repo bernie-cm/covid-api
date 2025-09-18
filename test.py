0383f697ef9c4b192f661ea4d97de4d3c85d639e
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-09-19 09:39:37 +1000

    Refactor code to add local CSV copy
    Add code to write a local CSV using the remot URL
    so Pandas can process a local copy.

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import requests
 
 url = "https://catalog.ourworldindata.org/garden/covid/latest/cases_deaths/cases_deaths.csv"
+csv_path = "/tmp/covid.csv"
 
-df = pd.read_csv(url)
+r = requests.get(url)
+with open(csv_path, 'wb') as fin:
+    fin.write(r.content)
+
+df = pd.read_csv(csv_path)
 print(df.head())
 print(df.tail())
 print(df.columns)
