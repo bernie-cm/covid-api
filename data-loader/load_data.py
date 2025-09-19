68ec27350be7e92af8a19e85baca2de20dc7d577
bernie-cm <170481602+bernie-cm@users.noreply.github.com>
2025-09-19 10:25:08 +1000

    Wrote requirements and docker compose
    Add several files to the repo, including docker compose
    and requirements needed for the build.

--- data-loader/load_data.py
+++ data-loader/load_data.py
@@ -1,5 +1,23 @@
 import pandas as pd
 import requests
+from sqlalchemy import create_engine
+import time
+import os
+
+# Stand up PostgreSQL environment variables and connection
+db_host = os.getenv('DB_HOST', 'db')
+db_user = os.getenv('DB_USER', 'covid')
+db_pass = os.getenv('DB_PASS', 'covid')
+db_name = os.getenv('DB_NAME', 'covid_data')
+
+engine = None
+while engine is None:
+    try:
+        engine = create_engine(f'postgresql://{db_user}:{db_pass}@{db_host}:5432/{db_name}')
+        engine.connect()
+    except Exception as e:
+        print('Waiting for PostgreSQL to be ready...')
+        time.sleep(3)
 
 url = "https://catalog.ourworldindata.org/garden/covid/latest/cases_deaths/cases_deaths.csv"
 csv_path = "/tmp/covid.csv"
@@ -9,6 +27,5 @@ with open(csv_path, 'wb') as fin:
     fin.write(r.content)
 
 df = pd.read_csv(csv_path)
-print(df.head())
-print(df.tail())
-print(df.columns)
+df.to_sql('covd_data', engine, if_exists='replace', index=False)
+print('Data loaded successfully!')
